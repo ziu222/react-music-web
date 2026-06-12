632f3f8bc1ec636c19ffdf44d7e2e59d839991a4
DESKTOP-GSPLG6E\Nghia <btn2812@gmail.com>
2026-06-12 21:10:12 +0700

    feat(admin): add listener stats mock data

---
+++ src/data/listenerStats.js
@@ -0,0 +1,30 @@
+export const listenerStats = [
+  {
+    userId: 1,
+    songsListened: 1247,
+    totalHours: 312,
+    topGenres: ["Pop", "R&B", "Lo-fi"],
+    topArtists: ["Sơn Tùng M-TP", "IU", "Taylor Swift"],
+  },
+  {
+    userId: 2,
+    songsListened: 384,
+    totalHours: 89,
+    topGenres: ["K-Pop", "Indie", "Acoustic"],
+    topArtists: ["BLACKPINK", "NewJeans", "Hòa Minzy"],
+  },
+  {
+    userId: 3,
+    songsListened: 2103,
+    totalHours: 518,
+    topGenres: ["V-Pop", "Ballad", "EDM"],
+    topArtists: ["Sơn Tùng M-TP", "Mỹ Tâm", "Đen Vâu"],
+  },
+  {
+    userId: 5,
+    songsListened: 91,
+    totalHours: 22,
+    topGenres: ["Hip-Hop", "Rap", "Trap"],
+    topArtists: ["Đen Vâu", "Rap Việt", "16 Typh"],
+  },
+];
